15a6b2a07aa2ca13fda2b73016dccc73aceaac10
diaba <hwar@cincinnatistate.edu>
2022-01-11 12:33:56 -0500

    add types of typescript

---
+++ types.ts
@@ -0,0 +1,20 @@
+//String
+let myName: string = 'James Bond';
+
+// number
+let size: number = 12;
+
+//boolean
+let isValid: boolean = true;
+
+// list string
+let listNames: string[] = ['Sadio Mane', 'Youssou Ndour'];
+
+// function with return
+function getName(): string {
+    return 42;
+  }
+  // no return
+  function noReturn(): void {
+    console.log('yo, whatsup?');
+  }
