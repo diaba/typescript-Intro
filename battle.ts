43cb80dcc79eef044e599f3691a95f22b384df80
diaba <hwar@cincinnatistate.edu>
2022-01-11 12:18:35 -0500

    add hello

---
+++ battle.ts
@@ -0,0 +1,22 @@
+var numberOfOrcs: number = 100;
+var numberOfHumans: number = 100;
+
+const humanAttackPower: number = 2;
+const orcAttackPower: number = 3;
+
+const humanBattlecry = 'For the alliance!';
+const orcBattlecry = 'For the horde';
+
+function makeAttack(quantity: number, power: number, battlecry: string) {
+  return function() {
+    console.log(battlecry);
+    quantity = quantity - power;
+  };
+}
+
+const orcAttack = makeAttack(numberOfHumans, orcAttackPower, orcBattlecry);
+const humanAttack = makeAttack(numberOfOrcs, humanAttackPower, humanBattlecry);
+
+orcAttack();
+humanAttack();
+
